328ab093faf65900af71e0f88889f32269feb51f
Baphomet <Hyperwave57@gmail.com>
2024-09-04 16:34:55 -0500

    Update mp3 2 mp4.bat

--- mp3 2 mp4.bat
+++ mp3 2 mp4.bat
@@ -11,7 +11,9 @@ echo where do you want the file bark
 set /p mp4=
 echo what do you want the file to be called bark
 set /p filename=
-ffmpeg -r 1 -loop 1 -y -i "%album%" -i "%mp3%" -c:a copy -r 1 -vcodec libx264 -s 1080*1080 -shortest "%mp4%\%filename%.mp4"
+echo what resolution do you want the video to be? (only input 1 number, it will be a square)
+set /p res=
+ffmpeg -r 1 -loop 1 -y -i "%album%" -i "%mp3%" -c:a copy -r 1 -vcodec libx264 -s %res%*%res% -shortest "%mp4%\%filename%.mp4"
 
 echo done bark bark!
 choice /C 12 /m "would you like to convert another bark? 1=y 2=n"
